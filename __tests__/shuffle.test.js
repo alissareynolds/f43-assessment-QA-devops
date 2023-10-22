7247c07dc1ce355d974dc3a37836c3143f59adf9
Alissa Reynolds <alissa6710@gmail.com>
2023-10-21 22:18:08 -0500

    finished the unit tests

--- __tests__/shuffle.test.js
+++ __tests__/shuffle.test.js
@@ -2,5 +2,21 @@ const shuffle = require("../src/shuffle");
 
 describe("shuffle should...", () => {
   // CODE HERE
-  test('')
+  test('return an array of same length', () => {
+    let array = ['a', 'b', 'c'];
+    let shuffledArray = shuffle(array);
+    expect(shuffledArray.length).toEqual(array.length);
+  });
+
+  test('return an array thats shuffled', () => {
+    let array = ['a', 'b', 'c'];
+    let shuffledArray = shuffle(array);
+    expect(shuffledArray[0] === array[0] && shuffledArray[1] === array[1] && shuffledArray[2] === array[2]).toEqual(false);
+  });
+
+  test('return an array of the same elements', () => {
+    let array = ['a', 'b', 'c'];
+    let shuffledArray = shuffle(array);
+    expect(shuffledArray.sort()).toStrictEqual(array.sort())
+  })
 });
